60a5ba16891b73e8f7223c639e1b7056610f773c
曾泳 <zengyongsun@163.com>
2018-04-28 17:52:45 +0800

    添加第三方库

--- app/src/main/java/com/example/administrator/functiondemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/functiondemo/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.administrator.functiondemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView) RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
